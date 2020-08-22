6c6199d81d8a5d99e5d5b552e5cc91359f5ec8d8
geekmdtravis <travis@geekmd.io>
2020-08-21 22:53:27 -0700

    Annotated w/ link to equation

--- src/ascvd/framingham.js
+++ src/ascvd/framingham.js
@@ -2,6 +2,7 @@
 
 import type { ASCVDData, FraminghamResult } from '../types/ASCVDData'
 
+// https://www.ahajournals.org/doi/10.1161/circulationaha.107.699579
 const framingham = (data: ASCVDData): FraminghamResult => {
   const badResult: FraminghamResult = {
     tenYearRisk: -1,
@@ -11,3 +12,15 @@ const framingham = (data: ASCVDData): FraminghamResult => {
 }
 
 export { framingham }
+
+const agePoints = (data: ASCVDData): number => {
+  return data.isGeneticMale ? agePointsMale(data) : agePointsFemale(data)
+}
+
+const agePointsFemale = (data: ASCVDData): number => {
+  throw new Error('Not implemented')
+}
+
+const agePointsMale = (data: ASCVDData): number => {
+  throw new Error('Not implemented')
+}
