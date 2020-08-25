7dc1e0e0e1588407895a771ba239d69c1690bf27
geekmdtravis <travis@geekmd.io>
2020-08-24 22:30:45 -0700

    [skip ci] - #10 implemented homa-IR

--- tests/homaIr.test.js
+++ tests/homaIr.test.js
@@ -2,8 +2,8 @@ import { homaIR } from './../src/homaIR'
 
 const variance = 0.1
 
-describe('homaIR test', () => {
-  it('calculates the score appropriately', () => {
+describe('homaIR', () => {
+  it('calculates the score appropriately within a reasonable variance', () => {
     const fastingInsulin = 12.3
     const fastingGlucose = 104
     const actual = homaIR(fastingInsulin, fastingGlucose)
