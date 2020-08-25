30bef90de067499012918e011a642a50cb5f7e7d
geekmdtravis <travis@geekmd.io>
2020-08-24 21:53:55 -0700

    [skip ci] - #1 - Created object to hold coefficients for the pooled cohort equation 2013.

--- src/ascvd/pooledCohort2013.js
+++ src/ascvd/pooledCohort2013.js
@@ -1,6 +1,7 @@
 // @flow
 import type { ASCVDData, PooledCohort2013Result } from '../types/ASCVDData'
 
+//https://www.onlinejacc.org/content/63/25_Part_B/2935/T6
 const pooledCohort2013 = (data: ASCVDData): PooledCohort2013Result => {
   const badResult: PooledCohort2013Result = {
     tenYearRisk: -1,
